47eca95d4fba71b0edc9217359be31347c3fe2a5
Oleksandr Kozaruk <alcosar@mail.ru>
2012-11-23 18:21:30 -0600

    platforms: lm4f: Add platform make file for Stellaris-launchpad.

---
+++ os/hal/platforms/LM4F/platform.mk
@@ -0,0 +1,7 @@
+# List of all the LM4F platform files.
+PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/LM4F/hal_lld.c \
+              ${CHIBIOS}/os/hal/platforms/LM4F/pal_lld.c \
+              ${CHIBIOS}/os/hal/platforms/LM4F/serial_lld.c \
+
+# Required include directories
+PLATFORMINC = ${CHIBIOS}/os/hal/platforms/LM4F
